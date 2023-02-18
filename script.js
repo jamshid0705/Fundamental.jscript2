fba7990d81994c6b0f274223cf73602afbdcb749
jamshid <jamshidshamshod0705@gmail.com>
2023-02-18 22:03:49 +0500

    code challenge

--- script.js
+++ script.js
@@ -43,16 +43,38 @@
 // console.log(b);
 
 ////////////////// 36 functions calling other functions ////////////////
-function cutFruit(fruit){
-  return fruit * 2;
+// function cutFruit(fruit){
+//   return fruit * 2;
+// }
+
+// function fruitProcessor(apple,orange){
+//   const fruitApple=cutFruit(apple)
+//   const fruitOrange=cutFruit(orange)
+//   return `You have got ${fruitApple} apples and ${fruitOrange} oranges !`
+// }
+
+// console.log(fruitProcessor(3,4))
+
+// /////////////// 37 functions //////////////////
+// const personAge=function(age){
+//   return 2023-age
+// }
+
+//////////////////// 37 coding challenge 1 /////////////////
+
+const calcAverage=(score1,score2,score3)=>(score1+score2+score3)/3
+
+function checkWinner(data1,data2,data3,data4,data5,data6){
+  const avgDolhins=calcAverage(data1,data2,data3)
+  const avgKoalas=calcAverage(data4,data5,data6)
+  if(avgDolhins>=2*avgKoalas){
+    return `Dolhins win🎉 (${avgDolhins} vs ${avgKoalas})`
+  }else if(avgDolhins*2<=avgKoalas){
+    return `Koalas win🎉 (${avgKoalas} vs ${avgDolhins})`;
+  } else{
+    return `Ohterwise no team wins ! (Koalas ${avgKoalas} vs Dolhins ${avgDolhins})`;
+  }
 }
 
-function fruitProcessor(apple,orange){
-  const fruitApple=cutFruit(apple)
-  const fruitOrange=cutFruit(orange)
-  return `You have got ${fruitApple} apples and ${fruitOrange} oranges !`
-}
-
-console.log(fruitProcessor(3,4))
-
-/////////////// 37 
+console.log(checkWinner(44,23,71,65,54,49))
+console.log(checkWinner(85,54,41,23,34,27))
