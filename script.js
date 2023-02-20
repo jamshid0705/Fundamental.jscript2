8ce97e92a3adae11e27555217e22ab265a8cc004
jamshid <jamshidshamshod0705@gmail.com>
2023-02-20 21:15:31 +0500

    coding

--- script.js
+++ script.js
@@ -330,24 +330,39 @@
 
 //////////////////// 50 coding challenge ////////////
 
-const calcTip = (bill) =>
-  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = (bill) =>
+//   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const total = [];
-for (let i = 0; i < bill.length; i++) {
-  tips.push(calcTip(bill[i]));
-  total.push(bill[i] + calcTip(bill[i]));
-}
-console.log(tips);
-console.log(total);
+// const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const total = [];
+// for (let i = 0; i < bill.length; i++) {
+//   tips.push(calcTip(bill[i]));
+//   total.push(bill[i] + calcTip(bill[i]));
+// }
+// console.log(tips);
+// console.log(total);
 
-const calsAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-};
-console.log(calsAverage(total));
+// const calsAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
+// console.log(calsAverage(total));
+
+////////////////////////// Coding challenge /////////////////
+// function sleepIn(weekday, vacation) {
+//   if (weekday == true && vacation == true) {
+//     return true;
+//   } else if (weekday == true && vacation == false) {
+//     return false;
+//   } else if (weekday == false && vacation == true) {
+//     return true;
+//   } else {
+//     return true;
+//   }
+// }
+
+///////////////////////////////////////////////////////////////////////
